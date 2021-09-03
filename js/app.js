94427706b74afb5600e9fc8586aa11dbaa3a0561
Administrator <admin@example.com>
2021-09-03 11:49:17 +0200

    3. Valida formulario

--- js/app.js
+++ js/app.js
@@ -15,5 +15,29 @@ function eventListeners() {
 function agregarTweet(e) {
     e.preventDefault();
 
+    //TextArea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+
+    //Validación
+    if(tweet === ''){
+        mostrarError('El mensaje no puede ir vacío');
+        return; //Evita que se ejecuten más líneas de código
+    }
     console.log('Agregando tweet');
+}
+
+//Mostrar error
+function mostrarError(error){
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    //Insertarlo en el contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    //Elimina la alerta después de 3 segundos
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
 }
